ea2f30214ddc88dc3d9bbd5bde55e6cccc7a7f7a
kassahun <kassuhan@gmail.com>
2023-03-17 14:06:16 +0200

    modified

--- Ecom-backend/routes/userRoutes.js
+++ Ecom-backend/routes/userRoutes.js
@@ -3,22 +3,22 @@ const User = require('../models/User');
 const Order = require('../models/Order');
 // signup
 
-router.post('/signup', async(req, res)=> {
-  const {name, email, password} = req.body;
+router.post('/signup', async (req, res) => {
+  const { name, email, password } = req.body;
 
   try {
-    const user = await User.create({name, email, password});
+    const user = await User.create({ name, email, password });
     res.json(user);
   } catch (e) {
-    if(e.code === 11000) return res.status(400).send('Email already exists');
+    if (e.code === 11000) return res.status(400).send('Email already exists');
     res.status(400).send(e.message)
   }
 })
 
 // login
 
-router.post('/login', async(req, res) => {
-  const {email, password} = req.body;
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
   try {
     const user = await User.findByCredentials(email, password);
     res.json(user)
@@ -29,7 +29,7 @@ router.post('/login', async(req, res) => {
 
 // get users;
 
-router.get('/', async(req, res)=> {
+router.get('/', async (req, res) => {
   try {
     const users = await User.find({ isAdmin: false }).populate('orders');
     res.json(users);
@@ -40,8 +40,8 @@ router.get('/', async(req, res)=> {
 
 // get user orders
 
-router.get('/:id/orders', async (req, res)=> {
-  const {id} = req.params;
+router.get('/:id/orders', async (req, res) => {
+  const { id } = req.params;
   try {
     const user = await User.findById(id).populate('orders');
     res.json(user.orders);
@@ -50,8 +50,8 @@ router.get('/:id/orders', async (req, res)=> {
   }
 })
 // update user notifcations
-router.post('/:id/updateNotifications', async(req, res)=> {
-  const {id} = req.params;
+router.post('/:id/updateNotifications', async (req, res) => {
+  const { id } = req.params;
   try {
     const user = await User.findById(id);
     user.notifications.forEach((notif) => {
