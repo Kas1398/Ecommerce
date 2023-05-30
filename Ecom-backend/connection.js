417a222d3cfb2e5cbdc24b6ea5742f4e5660d97c
kassahun <kassuhan@gmail.com>
2023-05-30 13:46:40 +0300

    connection fix

--- Ecom-backend/connection.js
+++ Ecom-backend/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-const connectionStr = "mongodb+srv://Ourproject:kassuhandoro@cluster0.ge9flii.mongodb.net/?retryWrites=true&w=majority";
+const connectionStr = process.env.MONGO_URI;
 
 mongoose.connect(connectionStr, { useNewUrlparser: true })
   .then(() => console.log('connected to mongodb'))
